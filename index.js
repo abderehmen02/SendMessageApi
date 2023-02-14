00f9d1e99a73fa651d74b07205cf788fba0fd634
abderehmen02 <ilahalaha0987@gmail.com>
2023-02-14 21:04:25 +0100

    fixing the express app errors

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+const { StatusCodes } = require("http-status-codes");
+const nodemailer = require("nodemailer");
+require("dotenv")
 const express = require("express")
-const nodemailer  = require("nodemailer")
 const app = express()
+// send me an email when someone visit Day After Day
 
-const port = process.env.PORT || 5000 ;
+const sendVisitingEmail = async (req , res)=>{
+
+    let transporter = nodemailer.createTransport({
+        service : 'gmail'  ,
+        auth : {
+            user : process.env.EMAIL_SENDER ,
+            pass :  process.env.EMAIL_SENDER_PASSWORD
+        }
+});
+ await transporter.sendMail({
+    from: process.env.EMAIL_SENDER,
+    to: process.env.EMAIL_RECEIVER,
+    subject: 'visit day after day',
+    text:  ` durration : ${req.body.durration}`
+}, (err, info) => {
+    if(info){
+        res.status(StatusCodes.OK)
+}
+    if(err)    console.log(err)
+});
+}
+const PORT = process.env.PORT || 2000
 
 
-app.listen(port , ()=>{
-    console.log(`app is running on port ${port}`)
-})
+
+
+app.get("/" , (req , res)=>{ res.send("hello world") })
+app.listen(PORT ,()=>console.log("app listening on port" + PORT) )
